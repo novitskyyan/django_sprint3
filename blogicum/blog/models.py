b407b5052d0698245459de765dec5d60e81638bf
Стас novitskyyan² <novitskyyan@gmail.com>
2023-10-10 23:41:50 +0300

    The first iteration

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -6,33 +6,49 @@ User = get_user_model()
 
 
 class BaseModel(models.Model):
-    is_published = models.BooleanField(verbose_name='Опубликовано',
-                                       default=True,
-                                       help_text='Снимите галочку, '
-                                                 'чтобы скрыть публикацию.'
+    is_published = models.BooleanField(
+        verbose_name='Опубликовано',
+        default=True,
+        help_text='Снимите галочку, '
+                  'чтобы скрыть публикацию.'
                                        )
-    created_at = models.DateTimeField(verbose_name='Добавлено', auto_now_add=True)
+    created_at = models.DateTimeField(
+        verbose_name='Добавлено',
+        auto_now_add=True)
 
     class Meta:
         abstract = True
 
 
 class Post(BaseModel):
-    title = models.CharField('Заголовок', max_length=256)
-    text = models.TextField(verbose_name='Текст')
-    pub_date = models.DateTimeField(verbose_name='Дата и время публикации',
-                                    help_text='Если установить дату и '
-                                              'время в будущем — '
-                                              'можно делать отложенные публикации.'
+    title = models.CharField(
+        'Заголовок', max_length=256)
+    text = models.TextField(verbose_name='Текст'
+                            )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text='Если установить дату и '
+                  'время в будущем — '
+                  'можно делать отложенные публикации.'
                                     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts', verbose_name='Автор публикации'
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
-        "Location", on_delete=models.SET_NULL, null=True, related_name='posts', verbose_name='Местоположение'
+        "Location", on_delete=models.SET_NULL,
+        null=True,
+        related_name='posts',
+        verbose_name='Местоположение'
     )
     category = models.ForeignKey(
-        "Category", on_delete=models.SET_NULL, null=True, related_name='posts', verbose_name='Категория'
+        "Category",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='posts',
+        verbose_name='Категория'
     )
 
     class Meta:
@@ -41,11 +57,18 @@ class Post(BaseModel):
 
 
 class Category(BaseModel):
-    title = models.CharField(verbose_name='Заголовок', max_length=256)
-    description = models.TextField(verbose_name='Описание')
-    slug = models.SlugField(verbose_name='Идентификатор', unique=True,
-                            help_text='Идентификатор страницы для URL; '
-                                      'разрешены символы латиницы, цифры, дефис и подчёркивание.'
+    title = models.CharField(
+        verbose_name='Заголовок',
+        max_length=256
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор',
+        unique=True,
+        help_text='Идентификатор страницы для URL; '
+                  'разрешены символы латиницы, цифры, дефис и подчёркивание.'
                             )
 
     class Meta:
@@ -57,7 +80,9 @@ class Category(BaseModel):
 
 
 class Location(BaseModel):
-    name = models.CharField(max_length=256, verbose_name='Название места')
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название места')
 
     class Meta:
         verbose_name = 'местоположение'
