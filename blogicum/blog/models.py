ad9080b1c341c803d22d8e78b190523c49521ada
Стас novitskyyan² <novitskyyan@gmail.com>
2023-10-22 14:49:00 +0300

    The second iteration

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
 from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+from . import constants
 
 User = get_user_model()
+
+
+class PostManager(models.Manager):
+    def get_queryset(self):
+        return Post.objects.filter(
+            category__is_published=True,
+            is_published=True,
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')
 
 
 class BaseModel(models.Model):
@@ -14,7 +26,8 @@ class BaseModel(models.Model):
     )
     created_at = models.DateTimeField(
         verbose_name='Добавлено',
-        auto_now_add=True)
+        auto_now_add=True
+    )
 
     class Meta:
         abstract = True
@@ -22,9 +35,12 @@ class BaseModel(models.Model):
 
 class Post(BaseModel):
     title = models.CharField(
-        'Заголовок', max_length=256)
-    text = models.TextField(verbose_name='Текст'
-                            )
+        'Заголовок',
+        max_length=constants.MAX_LENGTH
+    )
+    text = models.TextField(
+        verbose_name='Текст'
+    )
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
         help_text='Если установить дату и '
@@ -38,8 +54,10 @@ class Post(BaseModel):
         verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
-        "Location", on_delete=models.SET_NULL,
+        "Location",
+        on_delete=models.SET_NULL,
         null=True,
+        blank=True,
         related_name='posts',
         verbose_name='Местоположение'
     )
@@ -50,16 +68,19 @@ class Post(BaseModel):
         related_name='posts',
         verbose_name='Категория'
     )
+    objects = models.Manager()
+    post_objects = PostManager()
 
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
+        ordering = ['pub_date']
 
 
 class Category(BaseModel):
     title = models.CharField(
         verbose_name='Заголовок',
-        max_length=256
+        max_length=constants.MAX_LENGTH
     )
     description = models.TextField(
         verbose_name='Описание'
@@ -81,8 +102,9 @@ class Category(BaseModel):
 
 class Location(BaseModel):
     name = models.CharField(
-        max_length=256,
-        verbose_name='Название места')
+        max_length=constants.MAX_LENGTH,
+        verbose_name='Название места'
+    )
 
     class Meta:
         verbose_name = 'местоположение'
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -6,30 +6,28 @@ from .models import Post, Category
 
 def index(request):
     template = 'blog/index.html'
-    posts = Post.objects.filter(
-        category__is_published=True, is_published=True,
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
+    posts = Post.post_objects.all()[:5]
     context = {'post_list': posts}
     return render(request, template, context)
 
 
 def post_detail(request, post_id):
     template = 'blog/detail.html'
-    post = get_object_or_404(
-        Post,
-        id=post_id, pub_date__lte=timezone.now(), is_published=True,
-        category__is_published=True
-    )
+    post = get_object_or_404(Post.post_objects, id=post_id)
     context = {'post': post}
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
-    category = get_object_or_404(Category, slug=category_slug,
-                                 is_published=True)
-    posts = category.posts.filter(is_published=True,
-                                  pub_date__lte=timezone.now())
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    posts = category.posts.filter(
+        is_published=True,
+        pub_date__lte=timezone.now()
+    )
     context = {'category': category, 'post_list': posts}
     return render(request, template, context)
